c4d8229c9d50a7319086afbd2c3334a0e2e9b176
pranav1217 <pranavsutar730@gmail.com>
2026-07-08 14:51:05 +0530

    Load student data from JSON and find topper

--- app.py
+++ app.py
@@ -1,2 +1,9 @@
-marks=[2,3,4,5,6]
-print(max(marks))
+import json
+
+with open("students.json", "r") as f:
+    students = json.load(f)
+
+topper = max(students, key=lambda x: x["marks"])
+
+print("Topper:", topper["name"])
+print("Marks:", topper["marks"])
